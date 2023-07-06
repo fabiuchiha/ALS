e206c1b66deff1b758d70a42d924b268a8bbecaa
Fábio Cruz <fabiocruz1998@gmail.com>
2023-07-06 15:15:33 +0100

    Added network binary

--- utils.py
+++ utils.py
@@ -11,6 +11,7 @@ import csv
 from itertools import combinations
 import statistics
 from datetime import datetime as dt
+from cdlib import algorithms, readwrite, evaluation, NodeClustering
 
 def isfloat(num):
     try:
@@ -27,4 +28,46 @@ def binaryToData(fileName):
     with open(fileName, 'rb') as object_file:
         data = pickle.load(object_file)
         return data
+    
+def averageDegree(network):
+    degrees = [val for (node, val) in network.degree()]
+    sum = 0
+    for d in degrees:
+        sum += d
+    return sum/len(degrees)
 
+def kin(network):
+    nodes = network.nodes()
+    sum = 0
+    for n in nodes:
+        sum += network.in_degree(n)
+    return sum/len(nodes)
+
+def kout(network):
+    nodes = network.nodes()
+    sum = 0
+    for n in nodes:
+        sum += network.out_degree(n)
+    return sum/len(nodes)
+
+def APL(network):
+    if (nx.is_directed(network)):
+        largestComponent = 0
+        largestAPL = 0
+        for C in (network.subgraph(c) for c in nx.weakly_connected_components(network)):
+            if largestComponent<len(C.nodes):
+                largestComponent = len(C.nodes)
+                apl = nx.average_shortest_path_length(C)
+                largestAPL = apl
+        return largestAPL
+    else:
+        for C in (network.subgraph(c) for c in nx.connected_components(network)):
+            return nx.average_shortest_path_length(C)
+
+def networkInfo(network):
+    print("Average degree:", averageDegree(network))
+    if (nx.is_directed(network)):
+        print("Internal average degree:", kin(network))
+        print("External average degree:", kout(network))
+    print("Clustering coefficient:", nx.average_clustering(network))
+    print("Average Path Length (highest value):", APL(network))
